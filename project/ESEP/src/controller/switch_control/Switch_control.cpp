1ecf7f5e34a0196119409a841fdfa16ca0de6fb9
_Lynx <ldrews@me.com>
2016-12-21 14:06:52 +0100

    'Merge' switchControl code from 'featSwitchControl'

---
+++ project/ESEP/src/controller/switch_control/Switch_control.cpp
@@ -0,0 +1,18 @@
+/*
+ * Switch_control.cpp
+ *
+ *  Created on: 14.12.2016
+ *      Author: aby520
+ */
+
+#include "Switch_control.h"
+
+Switch_control::Switch_control() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Switch_control::~Switch_control() {
+	// TODO Auto-generated destructor stub
+}
+
